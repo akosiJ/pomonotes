d0092cf85da3ea91b0817ce885f0861fe592e1de
akosij <akosij>
2025-11-27 19:53:04 +0800

    Initial commit, start of project, - install primeNG - install Tailwind - initialized project. tested and running

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Lara from '@primeuix/themes/lara';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Lara
+            }
+        })
   ]
 };
